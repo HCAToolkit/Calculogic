507554cca2930ad11f4ff199e65ad69299f9f123
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-03-27 03:48:00 +0000

    Basic plugin Structure

---
+++ calculogic.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Calculogic: Advanced Templates and Quizzes for Creative Minds and Creators
+ * Plugin URI: https://www.hcatoolkit.com
+ * Description: A WordPress plugin for creating advanced templates and quizzes tailored for creative minds and creators.
+ * Version: 1.0.0
+ * Author: Yuri Bara
+ * Author URI: https://www.hcatoolkit.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: calculogic
+ */
+
+// Prevent direct access to the file
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
