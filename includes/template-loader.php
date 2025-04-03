4c25021f1f3f211369f6125d750486d1b6aa0c8b
Yuri-bara <143827071+Yuri-bara@users.noreply.github.com>
2025-04-03 22:19:23 +0000

    support for post types

--- includes/template-loader.php
+++ includes/template-loader.php
@@ -17,6 +17,22 @@ function calculogic_load_custom_templates( $template ) {
         }
     }
 
+    // Check if the current post type is 'quiz'
+    if ( is_singular( 'quiz' ) ) {
+        $custom_template = plugin_dir_path( __FILE__ ) . '../templates/single-quiz.php';
+        if ( file_exists( $custom_template ) ) {
+            return $custom_template;
+        }
+    }
+
+    // Check if the current post type is 'template'
+    if ( is_singular( 'template' ) ) {
+        $custom_template = plugin_dir_path( __FILE__ ) . '../templates/single-template.php';
+        if ( file_exists( $custom_template ) ) {
+            return $custom_template;
+        }
+    }
+
     return $template;
 }
 add_filter( 'template_include', 'calculogic_load_custom_templates' );
